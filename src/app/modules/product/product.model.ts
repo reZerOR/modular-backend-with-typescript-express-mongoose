ecb1138cbe90542f43dfc792f4e63233fa80d409
reZerOR <gkraihan14k@gmail.com>
2024-05-22 21:12:32 +0600

    interface and mongoose model created for product

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  TProduct,
+  TProductInventory,
+  TProductVariant,
+} from "./product.interface";
+
+const productVariantSchema = new Schema<TProductVariant>({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+const productInventorySchema = new Schema<TProductInventory>({
+  inStock: { type: Boolean, required: true },
+  quantity: { type: Number, required: true },
+});
+
+const productSchema = new Schema<TProduct>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: [{ type: String, required: true }],
+  variants: [{ type: productVariantSchema, _id: false }],
+  inventory: { type: productInventorySchema, _id: false },
+});
+
+export const Product = mongoose.model<TProduct>('Product', productSchema);
